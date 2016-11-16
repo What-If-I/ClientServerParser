59b47ea2433513efd6b4b4c26868d9d2630a63e3
What-If-I <Denka0087@gmail.com>
2016-11-16 19:50:30 +0300

    initial commit

---
+++ refresh_tables.sql
@@ -0,0 +1,8 @@
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (1, 'http://google.com', null, 0);
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (2, 'http://youtube.com', null, 0);
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (3, 'http://yandex.ru', null, 0);
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (4, 'https://www.crummy.com/', null, 0);
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (5, 'https://pymotw.com', null, 0);
+INSERT OR REPLACE INTO websites (id, url, title, parsed) VALUES (6, 'https://www.analyticsvidhya.com', null, 0);
+
+DELETE FROM links;
